81c3149abb342192d1cc8959428cc8380086e7c9
Amrut Pardeshi <amrut.pardeshi@nuance.com>
2024-10-08 16:22:30 +0530

    Added component based calculator.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import style from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
+import Display from "./components/Display";
 
 function App() {
-  return <>Calculator</>;
+  return (
+    <div className={style.calculator}>
+      <div id="Calculator">
+        <Display />
+        <ButtonsContainer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
